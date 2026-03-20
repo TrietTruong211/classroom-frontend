6ead166e8aa4ea71c0d6b08636df72629565d089
Chris M <chrism@mindarc.com.au>
2026-03-20 16:54:03 +1000

    Accommodate arcjet error

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,6 +1,29 @@
 import {createDataProvider, CreateDataProviderOptions} from "@refinedev/rest";
 import { BACKEND_BASE_URL } from "@/constants";
 import { ListResponse } from "@/types";
+import { HttpError } from "@refinedev/core";
+
+if (!BACKEND_BASE_URL) {
+  throw new Error("Missing required environment variable: VITE_BACKEND_BASE_URL");
+}
+
+const buildHttpError = async (response: Response): Promise<HttpError> => {
+  let message = 'Request failed';
+
+  try {
+    const  payload = (await response.json()) as {message?: string};
+    if (payload.message) {
+      message = payload.message;
+    }
+  } catch {
+    // Ignore error while parsing JSON, use default message
+  }
+
+  return {
+    message,
+    statusCode: response.status,
+  };
+}
 
 const options: CreateDataProviderOptions = {
   getList: {
@@ -26,10 +49,12 @@ const options: CreateDataProviderOptions = {
       return params;
     },
     mapResponse: async (response) => {
+      if (!response.ok) throw await buildHttpError(response);
       const payload: ListResponse = await response.clone().json();
       return payload.data ?? []
     },
     getTotalCount: async (response) => {
+      if (!response.ok) throw await buildHttpError(response);
       const payload: ListResponse = await response.clone().json();
       return payload.pagination?.total ?? 0;
     }
